d13f88c3ac9b653c3f0c105f063f8e63cc30bd7a
“alerenna” <alessandrorennafm@gmail.com>
2025-02-13 18:24:05 +0800

    Edit an error and add equal length

--- snack-2/main.js
+++ snack-2/main.js
@@ -1,11 +1,10 @@
 const firstWord = prompt('Type a word')
 const secondWord = prompt('Type another word')
 
-console.log(firstWord);
-console.log(secondWord);
-
-if (firstWord.leghnt < secondWord.leghnt) {
-    console.log(firstWord);
+if (firstWord.length < secondWord.length) {
+    console.log(firstWord + ' ' + secondWord);
 } else if (secondWord.length < firstWord.length) {
-    console.log(secondWord);
+    console.log(secondWord + ' ' + firstWord);
+} else if (secondWord.length == firstWord.length) {
+    console.log('The words have the same lenght'); 
 }
