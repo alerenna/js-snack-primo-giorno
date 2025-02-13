ebb8ec2e8a4a3626fd90bb647f406fca2f36831f
“alerenna” <alessandrorennafm@gmail.com>
2025-02-13 19:50:49 +0800

    Add solution

--- snack-4/main.js
+++ snack-4/main.js
@@ -1,3 +1,15 @@
 /* Crea un array vuoto.
 Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array. */
+
+let number = []
+
+for (let i = 1; i <= 6; i++) {
+    let userNumber = Number(prompt(`Inserisci un numero: ${i}`))
+    
+    if (userNumber % 2 === 1) {
+        number.push(userNumber)
+    }
+}
+
+console.log('I numeri dispari inseriti sono: ' + number);
