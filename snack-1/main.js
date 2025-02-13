d3407d402ee8d05c77ad8fd35fb3a20e868e1659
“alerenna” <alessandrorennafm@gmail.com>
2025-02-13 17:29:22 +0800

    Rename repo

---
+++ snack-1/main.js
@@ -0,0 +1,9 @@
+/* 
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+*/
+
+let numbers = []
+
+prompt('Insert a number')
+prompt('Insert another number')
