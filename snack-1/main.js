c5dcd2914bd46d03cf7afeb15d1a389041e829bd
“alerenna” <alessandrorennafm@gmail.com>
2025-02-13 17:44:32 +0800

    Add equal option

--- snack-1/main.js
+++ snack-1/main.js
@@ -10,4 +10,6 @@ if (firstNumber > secondNumber) {
     console.log(firstNumber);
 } else if (secondNumber > firstNumber) {
     console.log(secondNumber);
+} else if (firstNumber === secondNumber)  {
+    console.log('The numbers are equal');
 }
