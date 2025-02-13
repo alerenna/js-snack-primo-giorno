9a67bbdd8d69d79b7961550c225f551b1f4cc6f6
“alerenna” <alessandrorennafm@gmail.com>
2025-02-13 19:24:20 +0800

    Edit

--- snack-3/main.js
+++ snack-3/main.js
@@ -2,20 +2,6 @@
 Il programma stampa la somma di tutti i numeri inseriti. */
 
 
-/* const number = []
-let somma = 0
-
-for(let i = 1; i <= 3; i++) {
-    number.push(prompt('Enter a number'));
-  }
-  
-  for(let i = 1; i <= 3; i++) {
-    somma += number[i] 
-  }
-
-  console.log(somma); */
-  
-
   let sum = 0
 
   for (let i = 1; i < 5; i++) {
